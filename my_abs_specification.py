269c7947170a605c69c6860da21b8e006ee199ee
Apala <apalapramanikraka@gmail.com>
2023-03-29 13:26:20 -0500

    UPDATES

--- my_abs_specification.py
+++ my_abs_specification.py
@@ -1,4 +1,7 @@
+import os
 from abc import ABCMeta
+from operations import DenseTimeInterpreter
+from exception import MonException
 
 class AbstractSpecification(object):
     __metaclass__ = ABCMeta
@@ -15,7 +18,7 @@ class AbstractSpecification(object):
         self.var_object_dict = dict()
         
         
-#######################################
+##########################################################
         
     @property
     def name(self):
@@ -58,6 +61,196 @@ class AbstractSpecification(object):
         
 ###############################################################
 
+
+    @property
+    def out_var(self):
+        return self.ast.out_var
+
+    @out_var.setter
+    def out_var(self, out_var):
+        pass
+
+    @property
+    def out_var_field(self):
+        return self.ast.out_var_field
+
+    @out_var_field.setter
+    def out_var_field(self, out_var_field):
+        pass
+
+    @property
+    def var_topic_dict(self):
+        return self.ast.var_topic_dict
+
+    @var_topic_dict.setter
+    def var_topic_dict(self, var_topic_dict):
+        pass
+
+    @property
+    def var_object_dict(self):
+        return self.ast.var_object_dict
+
+    @var_object_dict.setter
+    def var_object_dict(self, var_object_dict):
+        self.__var_object_dict = var_object_dict
+        self.ast.var_object_dict = self.var_object_dict
+
+    @property
+    def free_vars(self):
+        return self.ast.free_vars
+
+    @free_vars.setter
+    def free_vars(self, free_vars):
+        pass
+
+###############################################################
+
+    def set_var_topic(self, var_name, var_topic):
+        self.ast.set_var_topic(var_name, var_topic)
+
+    def parse(self):
+        self.ast.parse()
+        
+    def set_sampling_period(self, sampling_period=int(1), unit='s', tolerance=float(0.1)):
+        if hasattr(self, 'runtime_interpreter'):
+            if isinstance(self.runtime_interpreter, DenseTimeInterpreter):
+                self.runtime_interpreter.set_sampling_period(sampling_period, unit, tolerance)
+            else:
+                MonException('time_unit_transformer() allowed only dense time')
+
+    def get_sampling_frequency(self, sampling_period=int(1), unit='s', tolerance=float(0.1)):
+        if hasattr(self, 'runtime_interpreter'):
+            if isinstance(self.runtime_interpreter, DenseTimeInterpreter):
+                self.runtime_interpreter.get_sampling_frequency()
+            else:
+                MonException('time_unit_transformer() allowed only dense time')
+                
+    @property
+    def sampling_violation_counter(self):
+        if hasattr(self, 'online_interpreter'):
+            if isinstance(self.runtime_interpreter, DenseTimeInterpreter):
+                return self.runtime_interpreter.sampling_violation_counter
+            else:
+                MonException('only discrete time has sampling_violation_counter')
+                
+    @property
+    def sampling_tolerance(self):
+        if hasattr(self, 'online_interpreter'):
+            if isinstance(self.runtime_interpreter, DenseTimeInterpreter):
+                return self.runtime_interpreter.sampling_tolerance
+            else:
+                MonException('only discrete time has sampling_tolerance')
+                
+    ############################################################
+    
+    
+    @property
+    def publish_var(self):
+        return self.__publish_var
+
+    @publish_var.setter
+    def publish_var(self, publish_var):
+        self.__publish_var = publish_var
+
+    @property
+    def publish_var_field(self):
+        return self.__publish_var_field
+
+    @publish_var_field.setter
+    def publish_var_field(self, publish_var_field):
+        self.__publish_var_field = publish_var_field
+
+    
+    ##################################################################
+    
+    def add_input_var(self, input_var):
+        self.in_vars.add(input_var)
+
+    def remove_input_var(self, var):
+        self.in_vars.discard(var)
+
+    def add_output_var(self, output_var):
+        self.out_vars.add(output_var)
+
+    def remove_output_var(self, var):
+        self.out_vars.discard(var)
+
+    def add_op(self, op):
+        self.ops.add(op)
+        
+    def get_spec_from_file(self, path):
+        """Opens a text file and returns its content as a string
+        Parameters:
+            path : String - path to the filename
+        Returns
+            out : String - file content
+        """
+        out = None
+        if os.path.exists(path):
+            f = open(path, "r")
+            out = f.read()
+            f.close()
+        else:
+            raise MonException('The file {} does not exist.'.format(path))
+        return out
+        
+######################################################################################   
+
+## WORK ON THIS CLASS-------------------
+    
+class AbstractOnlineSpecification(AbstractSpecification):
+    def __init__(self, ast, runtimeInterpreter, pastifier=None):
+        AbstractSpecification.__init__(self, ast)
+        self.name = 'Abstract Runtime Specification'
+        self.runtime_interpreter = runtimeInterpreter
+        self.pastifier = pastifier
+
+    # forwarding pastify
+    def pastify(self):
+        self.ast = self.pastifier.pastify(self.ast)
+
+    # forwarding to interpreter
+    def update(self, *args, **kwargs):
+        if self.set_ast_flag != True:
+            self.runtime_interpreter.set_ast(self.ast)
+            self.set_ast_flag = True
+
+        
+        if isinstance(self.runtime_interpreter, AbstractDenseTimeOnlineInterpreter):
+            if len(args) == 0:
+                raise Exception()
+            elif len(args) == 1:
+                dataset = [args[0]]
+            else:
+                dataset = []
+                for i in args:
+                    dataset.append(i)
+            return self.runtime_interpreter.update(dataset)
+        
+        else:
+            pass
+
+    def final_update(self, *args, **kwargs):
+        if self.set_ast_flag != True:
+            self.runtime_interpreter.set_ast(self.ast)
+            self.set_ast_flag = True
+
+       
+        if len(args) == 0:
+            raise Exception()
+        elif len(args) == 1:
+            dataset = [args[0]]
+        else:
+            dataset = []
+            for i in args:
+                dataset.append(i)
+
+        return self.runtime_interpreter.final_update(dataset)
+
+    def reset(self):
+        self.runtime_interpreter.reset()
+
+    
 
 
     
