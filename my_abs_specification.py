48c3453b132228f2e953c4d66cded90e0344422a
Apala <apalapramanikraka@gmail.com>
2023-03-29 12:07:16 -0500

    abs spec

--- my_abs_specification.py
+++ my_abs_specification.py
@@ -13,3 +13,54 @@ class AbstractSpecification(object):
         self.var_topic_dict = dict()
         self.free_vars = set()
         self.var_object_dict = dict()
+        
+        
+#######################################
+        
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self,name):
+        self.__name = name
+        
+    @property
+    def spec (self):
+        return self.ast.spec
+    
+    @spec.setter
+    def spec(self,spec):
+        self.ast.spec
+        
+    
+#########################################################
+
+    def add_var(self,var):
+        self.ast.vars.add(var)    
+        
+    
+    def get_value(self, var_name):
+        return self.ast.get_value(var_name)
+    
+    
+    def add_sub_spec(self, sub_spec):
+        self.ast.add_sub_spec(sub_spec)
+        
+    def set_var_io_type(self, var, io_type):
+        self.ast.set_var_io_type(var, io_type)
+        
+    def declare_var(self, var_name, var_type):
+        self.ast.declare_var(var_name, var_type)
+        
+    def declare_const(self, const_name, const_type, const_val):
+        self.ast.declare_const(const_name, const_type, const_val)
+        
+###############################################################
+
+
+
+    
+    
+    
+        
